80d7349c8055814386421b3bed2136abca6e31fa
Leen Al Lababidi <leenlab2@gmail.com>
2021-11-03 23:34:44 -0400

    Added Login component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,1 +1,26 @@
-//stuff
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    /**
+     * Validates log-in information and directs user towards the Choose Action page
+     */
+    handleSubmit = () => {
+        console.log("Handle submit");
+    }
+
+    render() {
+        return (
+            <div>
+                Log In
+            </div>
+        );
+    }
+}
