97f3dfa72bda08af4513e7d491172adff85ccabe
Leen Al Lababidi <leenlab2@gmail.com>
2021-11-11 15:09:22 -0500

    Styled home page

--- src/components/chooseAction/ActionButton.js
+++ src/components/chooseAction/ActionButton.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import styles from './ActionContainer.module.scss';
 
 /**
  * A button that directs the user to a specific page, depending on what they want to do
  */
 export default function ActionButton(props) {
     return (
-        <div>
-            <Link to={props.path}>{props.buttonLabel}</Link>
-        </div>
+        <Link className={styles.actionButton} to={props.path}>
+            <p>{props.buttonLabel}</p>
+            <img src={props.icon} alt="Send Email icon"/>
+        </Link>
     );
 }
 
 ActionButton.propTypes = {
     path: PropTypes.string,
     buttonLabel: PropTypes.string,
+    icon: PropTypes.string,
 }
--- src/components/chooseAction/ActionContainer.js
+++ src/components/chooseAction/ActionContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ActionButton from "./ActionButton";
 import styles from './ActionContainer.module.scss';
+import send from '../../resources/send.png';
+import profile from '../../resources/profile.png';
 
 /**
  * A container component that acts as our home page. It contains buttons that
@@ -15,18 +17,24 @@ export class ActionContainer extends React.Component {
         }
     }
 
+    // Icons made by https://www.freepik.com
+    // TODO: add proper attribution
     render(){
         return (
             <div className={styles.actionContainer}>
-                Choose An Action
-                <ActionButton
-                    path={this.state.launchWidgetPath}
-                    buttonLabel="Launch Targeted Widgets"
-                />
-                <ActionButton
-                    path={this.state.clientViewPath}
-                    buttonLabel="View Client Information"
-                />
+                <h1>Choose An Action</h1>
+                <div className={styles.buttons}>
+                    <ActionButton
+                        path={this.state.launchWidgetPath}
+                        buttonLabel="Launch Targeted Widgets"
+                        icon={send}
+                    />
+                    <ActionButton
+                        path={this.state.clientViewPath}
+                        buttonLabel="View Client Information"
+                        icon={profile}
+                    />
+                </div>
             </div>
         );
     }
