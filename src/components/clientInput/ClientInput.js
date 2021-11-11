4e987fae4c647b3b2b0cb8bd0603a69fe50847ba
Leen Al Lababidi <leenlab2@gmail.com>
2021-11-11 15:59:41 -0500

    Added elements of the input form

--- src/components/clientInput/ClientInput.js
+++ src/components/clientInput/ClientInput.js
@@ -19,7 +19,17 @@ class ClientInput extends React.Component {
     render() {
         return (
             <div>
-                Insert Client IDs
+                <h1>Insert Client IDs</h1>
+                <form>
+                    <input type="text" placeholder="Type the ID numbers here, separated by spaces..."/><br/>
+                    <label htmlFor="clientFile">Or, upload as a .txt file: </label>
+                    <input type="file" name="clientFile" /><br />
+                    <input type="checkbox" name="email" />
+                    <label htmlFor="email">Generate email markup</label><br/>
+                    <input type="checkbox" name="widget" checked/>
+                    <label htmlFor="widget">Launch targeted widgets</label>
+                    <input type="submit" value="Go" />
+                </form>
             </div>
         );
     }
