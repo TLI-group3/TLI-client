855a95f1242dd8f7b62211bd457b373ef94e3387
infinityworld13 <teresita.becerra@mail.utoronto.ca>
2021-11-24 11:47:41 -0500

    Fixed issues around widget and bank page removal from repo

--- mock banking page/widget/slideshow.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CarSlides from './carSlides'
-
-class Slideshow extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {curr_slide: CarSlides};
-    }
-}
